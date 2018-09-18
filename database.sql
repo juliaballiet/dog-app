73040d8f337e65d2c663b6fff64b35649d526b5e
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-18 15:44:26 -0500

    updated database.sql file with mock data

--- database.sql
+++ database.sql
@@ -123,3 +123,34 @@ CREATE TABLE "activities_exercise" (
 	"activity_id" integer REFERENCES "activities" NOT NULL,
 	"exercise_id" integer REFERENCES "exercise" NOT NULL,
 );
+
+-- MOCK DATA:
+
+INSERT INTO "dogs" ("user_id", "name", "breed", "weight", "birthday")
+VALUES (1, 'Cosmia', 'Kishu Ken', 28, '2015-08-27'), ('kibble', 'Orijen', 'Six Fish', 1, 1), 
+('kibble', 'Farmina', 'Boar and Apple', 1, 1), ('kibble', 'Acana', 'Grasslands', 1, 1), 
+('kibble', 'Fromm', 'Game Bird', .75, 1);
+
+INSERT INTO "feedings" ("dog_id", "food_id", "date", "time")
+VALUES (2, 1, '2018-09-10', '18:00'), (2, 1, '2018-09-11', '09:00'), (2, 2, '2018-09-11', '18:00'), (2, 1, '2018-09-12', '08:00'), 
+(2, 2, '2018-09-12', '16:00'), (2, 2, '2018-09-13', '09:00'), (2, 3, '2018-09-13', '17:00'), (2, 1, '2018-09-14', '09:30'), 
+(2, 1, '2018-09-14', '19:00'), (2, 3, '2018-09-15', '08:45'), (2, 3, '2018-09-15', '17:00'), (2, 3, '2018-09-16', '09:00'), 
+(2, 4, '2018-09-16', '16:30'), (2, 1, '2018-09-17', '09:00'), (2, 4, '2018-09-17', '17:00');
+
+INSERT INTO "exercise" ("dog_id", "date", "duration", "notes")
+VALUES (2, '2018-09-10', 30, 'ran hard'), (2, '2018-09-11', 15, 'quick walk'), (2, '2018-09-12', 45, 'walked really well after flirt pole'),
+(2, '2018-09-13', 60, 'ignored all other dogs');
+
+INSERT INTO "exercise" ("dog_id", "date", "duration")
+VALUES (2, '2018-09-14', 30), (2, '2018-09-15', 25),
+(2, '2018-09-16', 30), (2, '2018-09-17', 50);
+
+INSERT INTO "activities" ("activity", "user_id")
+VALUES ('walk', 1), ('dog park', 1), ('flirt pole', 1), ('hiking', 1), ('run', 1), ('fetch', 1);
+
+INSERT INTO "activities_exercise" ("activity_id", "exercise_id")
+VALUES (2, 1), (1, 2), (3, 3), (1, 3), (2, 4), (4, 4), (5, 5), (6, 6), (1, 7), (2, 8);
+
+INSERT INTO "skills" ("name", "user_id")
+VALUES ('down', 1), ('sit pretty', 1), ('down-stay', 1), ('up', 1), ('stand', 1), ('off', 1);
+
---
+++ src/components/TrainingLogPage/TrainingLog/TrainingLog.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Axios from 'axios';
+
+const mapStateToProps = state => ({
+  user: state.user,
+  exerciseLog: state.exerciseLog
+});
+
+class ExerciseLog extends Component {
+    componentDidMount = () => {
+        this.getExerciseLog();
+    }
+
+    getExerciseLog = () => {
+        console.log('in getExerciseLog');
+        Axios({
+            method: 'GET',
+            url: '/log/exercise/' + this.props.id
+        }).then((response) => {
+            console.log('back from getExerciseLog with: ', response.data);
+            let action = {
+                type: 'EXERCISE_LOG',
+                payload: response.data
+            }
+            this.props.dispatch(action);
+        }).catch((error) => {
+            console.log('getExerciseLog error: ', error);
+            alert('getExerciseLog error');
+          })
+    }
+
+  render() {
+    return (
+      <div>
+        <ul>
+            {this.props.exerciseLog.map((entry, i) => {
+                return(
+                    <li key={i}>
+                        <div>
+                            <h4>{entry.activity} {entry.variety}</h4>
+                            fo {entry.duration} minutes on {entry.date}
+                            <p>Notes: {entry.notes}</p>
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(ExerciseLog);
