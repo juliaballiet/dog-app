90208f69a3549945111cea8166c16d8742aed0d0
Julia Balliet <julia.m.balliet@gmail.com>
2018-09-19 09:48:53 -0500

    food list GET route server side

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,57 +5,14 @@ const router = express.Router();
 /*
  * GET route
  */
-router.get('/feeding/:id', (req, res) => {
+router.get('/food', (req, res) => {
     if (req.isAuthenticated()) {
-        console.log('/log/feeding GET route', req.params.id);
-        const queryText = `SELECT "feedings".*, "foods"."brand", "foods"."variety" 
-            FROM "feedings"
-            JOIN "foods" ON "foods"."id" = "feedings"."food_id"
-            WHERE "dog_id" = $1;`;
-        pool.query(queryText, [req.params.id]).then((results) => {
+        console.log('/list/food GET route', req.user.id);
+        const queryText = `SELECT * FROM "foods" WHERE "user_id" = $1;`;
+        pool.query(queryText, [req.user.id]).then((results) => {
             res.send(results.rows);
         }).catch((error) => {
             console.log('/feeding GET route error: ', error);
-            res.sendStatus(500);
-        })
-    } else {
-        res.sendStatus(403);
-    }
-});
-
-router.get('/exercise/:id', (req, res) => {
-    if (req.isAuthenticated()) {
-        console.log('/log/exercise GET route', req.params.id);
-        const queryText = `SELECT * FROM "exercise" 
-        JOIN "activities_exercise" 
-        ON "activities_exercise"."exercise_id" = "exercise"."id"
-        JOIN "activities"
-        ON "activities"."id" = "activities_exercise"."activity_id"
-        WHERE "dog_id" = $1`;
-        pool.query(queryText, [req.params.id]).then((results) => {
-            res.send(results.rows);
-        }).catch((error) => {
-            console.log('/exercise GET route error: ', error);
-            res.sendStatus(500);
-        })
-    } else {
-        res.sendStatus(403);
-    }
-});
-
-router.get('/training/:id', (req, res) => {
-    if (req.isAuthenticated()) {
-        console.log('/log/training GET route', req.params.id);
-        const queryText = `SELECT * FROM "training" 
-        JOIN "skills_training" 
-        ON "skills_training"."training_id" = "training"."id"
-        JOIN "skills"
-        ON "skills"."id" = "skills_training"."skill_id"
-        WHERE "dog_id" = $1;`;
-        pool.query(queryText, [req.params.id]).then((results) => {
-            res.send(results.rows);
-        }).catch((error) => {
-            console.log('/training GET route error: ', error);
             res.sendStatus(500);
         })
     } else {
